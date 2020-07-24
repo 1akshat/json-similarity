0bcffb9d930b7e6e792d16d3438024035c4c4d87
akki <akshat@insync.ai>
2020-07-24 14:54:23 +0530

    add comments to helper

--- app/helpers/json_similarity_helper.rb
+++ app/helpers/json_similarity_helper.rb
@@ -1,13 +1,9 @@
 require "json"
 
 module JsonSimilarityHelper
-    
-    def similarity_score
-        (@matched_values.to_f / (@matched_values + @unmatched_values))
-    end
-    
-    def compare_hashes(hash1, hash2)
 
+    def compare_hashes(hash1, hash2)
+        # Should through an error if parameters are not HASH
         if !hash1.kind_of? (Hash) or !hash2.kind_of? (Hash)
             raise "Expected Hash Type."
         end
@@ -22,7 +18,7 @@ module JsonSimilarityHelper
         if hash2_keys.nil?
             hash2_keys = []
         end
-    
+        # Computing Iterable Hash: The Hash which is having greater number of keys
         if hash1_keys.length > hash2_keys.length
             iterable_hash = hash1
             comparable_hash = hash2
@@ -30,16 +26,18 @@ module JsonSimilarityHelper
             iterable_hash = hash2
             comparable_hash = hash1
         end
+
         iterable_hash.each do |key, value|
-    
+            # Check for Primitive Value in iterable Hash
             if (value.kind_of? (String) or value.kind_of? (Integer) or value.kind_of? (Float) or value.kind_of? (TrueClass) or value.kind_of? (FalseClass))
                 compare_primitives(key, value, comparable_hash)
             end
-    
+            # Check for array value in iterable Hash
             if value.kind_of? (Array)
                 compare_arrays(key, value, hash1, hash2)
             end
     
+            # Check for Hash value in iterable Hash
             if value.kind_of? (Hash)
                 if hash1[key].nil?
                     comparable_hash1 = {}
@@ -52,14 +50,19 @@ module JsonSimilarityHelper
                 else
                     comparable_hash2 = hash2[key]
                 end
+                # Recursive Call
                 compare_hashes(comparable_hash1, comparable_hash2)
             end
         end
+        # This returns similarity score function that return the value
         similarity_score
     end
     
+    # The parameters are key and value from the first hash and complete second hash
+    # variables with @ are global vars.
     def compare_primitives(key, value, hash2)
         unless hash2.nil?
+            # Check if Second Hash is containing the same key and value
             @matched_values += 1 if hash2[key] == value
             @unmatched_values += 1 if hash2[key] != value
         else
@@ -70,10 +73,13 @@ module JsonSimilarityHelper
     def compare_arrays(key, value, hash1, hash2)
         array_to_loop = []
         other_array = []
-        # value is an array
+        # value is an Array type, containing multiple hashes.
         value.each do |individual_obj|
+            # Individual obj is a hash
             if individual_obj.kind_of? (Hash)
+                # Possibility: Inside a hash there can be key with value type as Array
                 if hash1[key].kind_of? Array and hash2[key].kind_of? Array
+                    # Finding the array with greater length to loop with so that keys hashes are not missed while comparing
                     array1_length = hash1[key].length
                     array2_length = hash2[key].length
                     if array1_length > array2_length
@@ -97,24 +103,25 @@ module JsonSimilarityHelper
         end
         array_to_loop.each_with_index do |deep_object, index|
             if other_array and !other_array[index].nil?
+                # Finally got the hashes so now can compare those
                 compare_hashes(deep_object, other_array[index])
             else
+                # If one key value is missing in other hash then assuming that to be empty Hash
                 compare_hashes(deep_object, {})
             end
         end
     end
     
+    # This function expects a file path to get passed and return the json
     def read_file(file_name)
         file = File.open(file_name)
         data = JSON.load file
         return data
     end
 
-    # def compare_files(file1, file2)
-    #     file1_data = read_file(file1)
-    #     file2_data = read_file(file2)
-    
-    #     compare_hashes(file1_data, file2_data)
-    # end
+    def similarity_score
+        # Algo: Truthy/(Truthy + Falsy)
+        (@matched_values.to_f / (@matched_values + @unmatched_values))
+    end
 
 end
